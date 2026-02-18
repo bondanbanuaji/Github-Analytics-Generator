c559e696f4d9abcfa11fb03624391c3b415ea6d8
bondanbanuaji <bondanbanuaji@gmail.com>
2026-02-18 15:47:08 +0700

    update 24.x node js

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Xw54_Lpr.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/GitHubDashboard.D8sPPCpB.js
+++
@@ -1,1 +0,0 @@
-import{G as p}from"./GitHubDashboard.BEumwpyE.js";import"./with-selector.fG4FxtSD.js";import"./index.Dr7TU7rC.js";import"./index.CcHevvzP.js";export{p as GitHubDashboard};
